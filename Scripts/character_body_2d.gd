8661ed249ebe217b53f9a70c20a9dbd8a7e407c4
jaswanthsatyadev <jaswanthsatyadev55@gmail.com>
2025-01-22 13:25:42 +0530

    character bug fix
    fixing jumping animation bug

--- Scripts/character_body_2d.gd
+++ Scripts/character_body_2d.gd
@@ -1,8 +1,8 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const SPEED = 100.0
+const JUMP_VELOCITY = -300.0
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
 
@@ -12,6 +12,9 @@ func _physics_process(delta: float) -> void:
 	if velocity.x != 0 :
 		animated_sprite_2d.animation = "running"
 	
+	else :
+		animated_sprite_2d.animation = "idle"
+	
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
@@ -20,6 +23,7 @@ func _physics_process(delta: float) -> void:
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		animated_sprite_2d.animation = "jumping"
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
