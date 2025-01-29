3a33bc18d1f57c316268f1a6a731caa96f9c3ff0
Yogadharshan <yogadharshan007@gmail.com>
2025-01-29 18:22:45 +0530

    implemented enemy movement and chase

--- Scripts/rat.gd
+++ Scripts/rat.gd
@@ -1,52 +1,21 @@
 extends CharacterBody2D
 
-var SPEED = 50
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-var player
-var chase = false
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
-func _ready():
-	get_node("AnimatedSprite2D").play("Idle")
-func _physics_process(delta):
-	#Gravity for Frog
-	velocity.y += gravity * delta
-	if chase == true:
-		if get_node("AnimatedSprite2D").animation != "Death":
-			get_node("AnimatedSprite2D").play("Jump")
-		player = get_node("../../Player/Player")
-		var direction = (player.position - self.position).normalized()
-		if direction.x > 0:
-			get_node("AnimatedSprite2D").flip_h = true
-		else:
-			get_node("AnimatedSprite2D").flip_h = false
-		velocity.x = direction.x * SPEED
-	else:
-		if get_node("AnimatedSprite2D").animation != "Death":
-			get_node("AnimatedSprite2D").play("Idle")
-		velocity.x = 0
-	move_and_slide()
+var speed = 25
+var player_chase = false
+var player =  null
+
+func _physics_process(delta: float) -> void: 
+	if player_chase:
+		position += (player.position - position)/speed
+
+func _on_dectection_area_body_entered(body):
+	player = body
+	player_chase = true
+func _on_dectection_area_body_exited(body):
+	player = null
+	player_chase = false
+
+
 	
-func _on_player_detection_body_entered(body):
-	if body.name == "Player":
-		chase = true
-
-
-func _on_player_detection_body_exited(body):
-	if body.name == "Player":
-		chase = false
-
-
-func _on_player_death_body_entered(body):
-	if body.name == "Player":
-		death()
-func _on_player_collison_body_entered(body):
-	if body.name == "Player":
-		Game.playerHP -= 3
-		death()
-func death():
-	Game.Gold += 5
-	#Utils.saveGame()
-	chase = false
-	get_node("AnimatedSprite2D").play("Death")
-	await get_node("AnimatedSprite2D").animation_finished
-	self.queue_free()
